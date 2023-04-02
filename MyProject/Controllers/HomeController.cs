8125a04ebe057df379db55d2a6344bc1acee60a2
User <ke5891995@gmail.com>
2023-04-02 07:49:24 +0200

    Build project Structure

---
+++ MyProject/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult IS_Dept()
+        {
+
+            return View();
+        }
+        public ActionResult CS_Dept()
+        {
+
+            return View();
+        }
+        public ActionResult SC_Dept()
+        {
+
+            return View();
+        }
+        public ActionResult Medical_Informatics_Program()
+        {
+
+            return View();
+        }
+        public ActionResult Information_Security_Program()
+        {
+
+            return View();
+        }
+        public ActionResult PostGraduate_Section()
+        {
+            return View();
+        }
+        public ActionResult Community_Service()
+        {
+            return View();
+        }
+
+        public ActionResult Quality_Unit()
+        {
+            return View();
+        }
+
+        public ActionResult IT_Unit()
+        {
+            return View();
+        }
+
+
+    }
+}
